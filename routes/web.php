60d38b277bf9b0397860b902b2a2ce779005d6fd
Noha Shehab <nshehab@iti.gov.eg>
2023-10-02 11:58:48 +0300

    laravel intro

---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('iti', function(){
+
+    return "Hello World";
+});
+
+
+
+// Route::get('students', function(){
+//     $students = [
+//         "Mohamed", 'Moataz', 'Ahmed', 'Ali', 'Amany'
+//     ];
+//     return $students;
+// });
+
+
+
+Route::get('students', function(){
+    $students = [
+        ["id"=>"1", "name"=>"Ahmed"], 
+        ["id"=>"2", "name"=>"Mohamed"],
+        ["id"=>"3", "name"=>"Ali"]
+
+    ];
+    return $students;
+});
+
+
+# send id as a paramter to the url ? 
+# {} ==> this a mandatory changable part in the url
+Route::get('students/{id}', function($id){
+    $students = [
+        ["id"=>"1", "name"=>"Ahmed"], 
+        ["id"=>"2", "name"=>"Mohamed"],
+        ["id"=>"3", "name"=>"Ali"]
+
+    ];
+
+    foreach ($students as $std){
+            if ($std["id"]==$id){
+                return $std;
+            }
+    }
+    return abort(404);
+});
+
+
+
+
+
+
+
+
+
+
